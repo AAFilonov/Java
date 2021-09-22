e34d9b7ea37ee85b437e197130c123dcc6a30514
afilonov <filon13p@gmail.com>
2021-09-22 11:44:40 +0300

    Сделал ЛР1

--- LR1/src/main/java/Main.java
+++ LR1/src/main/java/Main.java
@@ -1,7 +1,23 @@
+/*2. Write a program that reads an integer angle (which may be positive or negative)
+        and normalizes it to a value between 0 and 359 degrees. Try it first with the %
+        operator, then with floorMod.
+        3. Using only the conditional operator, write a program that reads three integers and
+        prints the largest. Repeat with Math.max.
+        4. Write a program that prints the smallest and largest positive double values.
+        Hint: Look up Math.nextUp in the Java API*/
+
+
+import LR1.task2;
+import LR1.task3;
+import LR1.task4;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello java");
+    public static void main(String[] args) throws IOException {
+        task2.doTask();
+        //task3.doTask();
+        //task4.doTask();
     }
 }
 
